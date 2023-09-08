05122a74b30d14a4bae4a4b776968bdebd40321c
Ramos-3 <142635216+Ramos-3@users.noreply.github.com>
2023-09-08 13:53:02 -0500

    Add files via upload

---
+++ src/t1/java/Sujeto.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1.java;
+
+/**
+ *
+ * @author diego
+ */
+public class Sujeto {
+
+    Sujeto(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    private String nombre;
+    
+}
---
+++ src/t1/java/T1Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1.java;
+
+/**
+ *
+ * @author diego
+ */
+public class T1Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
